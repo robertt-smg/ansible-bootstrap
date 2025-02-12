cd667141c9a5f025c64f3463a56f60eecd104623
Robert Thurnreiter <robert.thurnreiter@schmetterling.de>
2025-02-12 16:24:00 +0100

    Update ssh-rsa

--- docs/bootstrap.sh
+++ docs/bootstrap.sh
@@ -43,7 +43,7 @@ function add_ansible_user() {
     chmod 440 /etc/sudoers.d/ansible
     mkdir -p -m 700 /home/ansible/.ssh
     #echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFsW/fNKMjMQjkYcQOqwD14UItgMBGIX7HHpP2YTvQkI ansible" > /home/ansible/.ssh/authorized_keys
-    wget https://thr27.github.io/la-cuna-icu-bootstrap/authorized_keys -O /home/ansible/.ssh/authorized_keys
+    wget https://robertt-smg.github.io/ansible-bootstrap/authorized_keys -O /home/ansible/.ssh/authorized_keys
     chmod 600 /home/ansible/.ssh/authorized_keys
     chown -R ansible:ansible /home/ansible/.ssh
 
